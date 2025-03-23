f7c8faf90630cfd282b48f8342db22280ef7aedf
tinatin92 <tinatygordadze@gmail.com>
2025-03-23 21:51:03 +0400

    fix animation

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,14 @@ ScrollReveal().reveal('.hero-header', {
     opacity: 0, 
       easing: 'ease-in-out',
     duration: 800 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.about-us__text', { 
     distance: '100%', 
     origin: 'left', 
     duration: 1000 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.about-us_header', { 
@@ -19,26 +19,26 @@ ScrollReveal().reveal('.about-us_header', {
     origin: 'right', 
     duration: 1600,
     // reset: true ,
-    mobile: false 
+    // mobile: false 
     
 });
 ScrollReveal().reveal('.okudzava-img .image', { 
     distance: '100px', 
     origin: 'left', 
     duration: 1000 ,
-    mobile: false 
+    // mobile: false 
 });
 ScrollReveal().reveal('.okudzava-img .okadzava-image_text', { 
     distance: '100px', 
     origin: 'bottom', 
     duration: 1000 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.okudzava-text_text', { 
     opacity: 0,
     duration: 2000 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.rel-est_list-item', { 
@@ -48,7 +48,7 @@ ScrollReveal().reveal('.rel-est_list-item', {
     opacity: 0, 
     easing: 'ease-in-out',
     interval: 200 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.bank-list_item', { 
@@ -58,7 +58,7 @@ ScrollReveal().reveal('.bank-list_item', {
     opacity: 0, 
     easing: 'ease-in-out',
     interval: 200 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.bank-text .h2', { 
@@ -66,13 +66,13 @@ ScrollReveal().reveal('.bank-text .h2', {
     origin: 'right', 
     distance: '100px', 
     duration: 2000 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.bank-icon ', { 
     opacity: 0,
     duration: 2000 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.deal-list_list-item', { 
@@ -82,22 +82,22 @@ ScrollReveal().reveal('.deal-list_list-item', {
     opacity: 0, 
     easing: 'ease-in-out',
     interval: 200 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.deal-img img', { 
     opacity: 0,
     origin: 'left', 
-    distance: '400px', 
+    distance: '100px', 
     duration: 2000 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.peko2-img1', { 
     distance: '100px', 
     origin: 'bottom', 
     duration: 1000 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.peko2-img3 img', { 
@@ -107,15 +107,15 @@ ScrollReveal().reveal('.peko2-img3 img', {
     opacity: 0, 
     easing: 'ease-in-out',
     interval: 200 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.peko-icon', { 
     opacity: 0,
     origin: 'left', 
-    distance: '400px', 
+    distance: '100%', 
     duration: 2000 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.globe-list_item', { 
@@ -125,15 +125,15 @@ ScrollReveal().reveal('.globe-list_item', {
     opacity: 0, 
     easing: 'ease-in-out',
     interval: 200 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.globe-img', { 
-    distance: '200px', 
+    distance: '50%px', 
     origin: 'right', 
     duration: 1000, 
     opacity: 0, 
-    mobile: false 
+    // mobile: false 
    
 });
 
@@ -141,11 +141,11 @@ ScrollReveal().reveal('.globe-img', {
 ScrollReveal().reveal('.news-box', { 
     opacity: 0,
     origin: 'bottom', 
-    distance: '400px', 
+    distance: '50%', 
     easing: 'ease-in-out',
     duration: 1000, 
     interval: 200 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.testimonial-box', { 
@@ -155,13 +155,13 @@ ScrollReveal().reveal('.testimonial-box', {
     easing: 'ease-in-out',
     duration: 1000, 
     interval: 200 ,
-    mobile: false 
+    // mobile: false 
 });
 
 ScrollReveal().reveal('.footer-img', { 
-    distance: '200px', 
+    distance: '50%', 
     origin: 'left', 
     duration: 2000, 
     opacity: 0, 
-    mobile: false 
+    // mobile: false 
 });
