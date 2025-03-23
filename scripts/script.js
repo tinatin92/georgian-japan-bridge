88915f59695f8a3d35b4d2b102343ed98beb4241
tinatin92 <tinatygordadze@gmail.com>
2025-03-23 21:05:49 +0400

    animations

---
+++ scripts/script.js
@@ -0,0 +1,140 @@
+
+
+ScrollReveal().reveal('.hero-header', { 
+    opacity: 0, 
+      easing: 'ease-in-out',
+    duration: 800 
+});
+
+ScrollReveal().reveal('.about-us__text', { 
+    distance: '100%', 
+    origin: 'left', 
+    duration: 1000 
+});
+
+ScrollReveal().reveal('.about-us_header', { 
+    distance: '100%', 
+    origin: 'right', 
+    duration: 1600,
+    // reset: true 
+    
+});
+ScrollReveal().reveal('.okudzava-img .image', { 
+    distance: '100px', 
+    origin: 'left', 
+    duration: 1000 
+});
+ScrollReveal().reveal('.okudzava-img .okadzava-image_text', { 
+    distance: '100px', 
+    origin: 'bottom', 
+    duration: 1000 
+});
+
+ScrollReveal().reveal('.okudzava-text_text', { 
+    opacity: 0,
+    duration: 2000 
+});
+
+ScrollReveal().reveal('.rel-est_list-item', { 
+    distance: '50px', 
+    origin: 'bottom', 
+    duration: 800, 
+    opacity: 0, 
+    easing: 'ease-in-out',
+    interval: 200 ,
+});
+
+ScrollReveal().reveal('.bank-list_item', { 
+    distance: '50px', 
+    origin: 'bottom', 
+    duration: 800, 
+    opacity: 0, 
+    easing: 'ease-in-out',
+    interval: 200 ,
+});
+
+ScrollReveal().reveal('.bank-text .h2', { 
+    opacity: 0,
+    origin: 'right', 
+    distance: '100px', 
+    duration: 2000 
+});
+
+ScrollReveal().reveal('.bank-icon ', { 
+    opacity: 0,
+    duration: 2000 
+});
+
+ScrollReveal().reveal('.deal-list_list-item', { 
+    distance: '50px', 
+    origin: 'bottom', 
+    duration: 800, 
+    opacity: 0, 
+    easing: 'ease-in-out',
+    interval: 200 ,
+});
+
+ScrollReveal().reveal('.deal-img img', { 
+    opacity: 0,
+    origin: 'left', 
+    distance: '400px', 
+    duration: 2000 
+});
+
+ScrollReveal().reveal('.peko2-img1', { 
+    distance: '100px', 
+    origin: 'bottom', 
+    duration: 1000 
+});
+
+ScrollReveal().reveal('.peko2-img3 img', { 
+    distance: '50px', 
+    origin: 'right', 
+    duration: 800, 
+    opacity: 0, 
+    easing: 'ease-in-out',
+    interval: 200 ,
+});
+
+ScrollReveal().reveal('.peko-icon', { 
+    opacity: 0,
+    origin: 'left', 
+    distance: '400px', 
+    duration: 2000 
+});
+
+ScrollReveal().reveal('.globe-list_item', { 
+    distance: '50px', 
+    origin: 'bottom', 
+    duration: 800, 
+    opacity: 0, 
+    easing: 'ease-in-out',
+    interval: 200 ,
+});
+
+ScrollReveal().reveal('.globe-img', { 
+    distance: '200px', 
+    origin: 'right', 
+    duration: 1000, 
+    opacity: 0, 
+   
+});
+
+
+ScrollReveal().reveal('.news-box', { 
+    opacity: 0,
+    origin: 'bottom', 
+    distance: '400px', 
+    easing: 'ease-in-out',
+    duration: 1000, 
+    interval: 200 ,
+});
+
+ScrollReveal().reveal('.testimonial-box', { 
+    opacity: 0,
+    origin: 'bottom', 
+    distance: '200px', 
+    easing: 'ease-in-out',
+    duration: 1000, 
+    interval: 200 ,
+});
